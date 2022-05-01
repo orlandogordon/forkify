858524df5bb61bbd3b348531e58498e07ddcc2d7
orlandogordon <ogordon1998@gmail.com>
2022-05-01 18:54:24 -0400

    Attempt to fix the fractional-library bug by replacing the functionality with a new class in the helper script

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View';
 
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+import { Fractional } from '../helpers';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -129,7 +129,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? new Fractional(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
