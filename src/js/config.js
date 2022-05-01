405a902a13af7376ed36db7631522b1d3a966663
orlandogordon <ogordon1998@gmail.com>
2022-05-01 17:26:10 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2432afd9-12b3-4b81-9133-5915449a9945';
+export const MODAL_CLOSE_SEC = 2.5;
